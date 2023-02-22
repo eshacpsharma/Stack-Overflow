a524fde070cc8618c5fe7fe1b582d4bf56f84e82
Esha Sharma <eshacpsharma@gmail.com>
2023-02-22 19:20:17 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router} from 'react-router-dom'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import Navbar from './components/Navbar/Navbar'
+import AllRoutes from './AllRoutes';
+import { fetchAllQuestions } from '../src/actions/question.js'
+import { fetchAllUsers } from '../src/actions/users.js'
+
+function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+  dispatch(fetchAllQuestions())
+  dispatch(fetchAllUsers())
+  }, [dispatch])
+
+  return (
+    <div className="App">
+      <Router >
+      <Navbar />
+      <AllRoutes/>
+     </Router >
+    </div>
+  );
+}
+
+export default App;
